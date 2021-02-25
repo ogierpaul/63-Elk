a350199a0c91723fffc0998de0747e20f9164d86
pogier <ogier.paul@gmail.com>
2021-02-25 09:12:34 +0100

    oneline

--- dockersetup/shared_volume/3_create_suricate_fdw.sql
+++ dockersetup/shared_volume/3_create_suricate_fdw.sql
@@ -6,8 +6,8 @@ OPTIONS (
 );
 
 
-DROP FOREIGN TABLE IF EXISTS fdwes.target_jq;
-CREATE FOREIGN TABLE IF NOT EXISTS fdwes.target_jq(
+DROP FOREIGN TABLE IF EXISTS fdwes.dsl_target;
+CREATE FOREIGN TABLE IF NOT EXISTS fdwes.dsl_target(
         pg_id INTEGER,
         query TEXT,
         response TEXT
@@ -31,81 +31,26 @@ OPTIONS
     )
 ;
 
--- SELECT pg_id, query, response
--- FROM fdwes.target_jq
--- WHERE query ='{"query" : {"bool" : {"should": [{"match": {"surname": {"query": "WALLS", "fuzziness": 1}}}, {"match": {"firstnames": {"query": "Kenneth Charle", "fuzziness": 1}}}, {"match": {"route": {"query": "Squirrel Rise", "fuzziness": 1}}}, {"match": {"locality": {"query": "Marlow", "fuzziness": 1}}}, {"match": {"postalcodelong": {"query": "SL7 3PN", "fuzziness": 0}}}]}}}' AND pg_id=1;
-
-DROP MATERIALIZED VIEW IF EXISTS fdwes.z_text;
-CREATE MATERIALIZED VIEW fdwes.z_text AS
+DROP MATERIALIZED VIEW IF EXISTS fdwes.dsl_query;
+CREATE MATERIALIZED VIEW fdwes.dsl_query AS
     -- nesting the array with the different terms in its proper place
 SELECT
-       row_id_source as pg_id,
-       body::TEXT as query
-FROM fdwes.z_final_body;
+       row_id AS pg_id,
+       json_build_object(
+           'query', json_build_object(
+               'bool', jsonb_build_object(
+                   'should',  jsar
+               )
+           )
+       )::TEXT as query
+FROM elasticqueries.c_array_wo_nulls LIMIT 7;
 
--- SELECT * FROM fdwes.z_text LIMIT 1;
-
-DROP FUNCTION IF EXISTS es_search(body TEXT, i INTEGER);
-CREATE OR REPLACE FUNCTION es_search(body TEXT, i INTEGER)
-RETURNS TABLE (pg_id INTEGER, query TEXT, response TEXT)
+-- SELECT * FROM fdwes.dsl_target
+-- WHERE query = '{"query" : {"bool" : {"should": [{"match": {"surname": {"query": "WALLS", "fuzziness": 1}}}, {"match": {"firstnames": {"query": "Kenneth Charle", "fuzziness": 1}}}, {"match": {"route": {"query": "Squirrel Rise", "fuzziness": 1}}}, {"match": {"locality": {"query": "Marlow", "fuzziness": 1}}}, {"match": {"postalcodelong": {"query": "SL7 3PN", "fuzziness": 0}}}]}}}' AND pg_id = 1;
+-- --
+DROP MATERIALIZED VIEW IF EXISTS fdwes.dsl_results;
+CREATE MATERIALIZED VIEW fdwes.dsl_results
 AS
-$$
-BEGIN
-    RAISE NOTICE 'Sql Notice: pg_id %', i;
-RETURN QUERY
-SELECT
-    b."pg_id",
-    b."query",
-    b."response"
-FROM
-    fdwes.target_jq b
-WHERE b."query"=body and b."pg_id" =i;
-END
-$$ LANGUAGE plpgsql STABLE PARALLEL SAFE STRICT ;
---
--- SELECT row_id_source,
---        body::TEXT
--- FROM fdwes.z_final_body LIMIT 5;
---SELECT COUNT(*) FROM es_search('{"query" : {"bool" : {"should": [{"match": {"surname": {"query": "WALLS", "fuzziness": 1}}}, {"match": {"firstnames": {"query": "Kenneth Charle", "fuzziness": 1}}}, {"match": {"route": {"query": "Squirrel Rise", "fuzziness": 1}}}, {"match": {"locality": {"query": "Marlow", "fuzziness": 1}}}, {"match": {"postalcodelong": {"query": "SL7 3PN", "fuzziness": 0}}}]}}}', 1);
-
--- SELECT * FROM fdwes.z_text;
---
-DROP TABLE IF EXISTS fdwes.raw_results;
-CREATE TABLE IF NOT EXISTS fdwes.raw_results as
-
-EXPLAIN (FORMAT JSON) SELECT f.pg_id, f.response
-FROM (SELECT * FROM fdwes.z_text ) t
-CROSS JOIN LATERAL es_search(t.query, t.pg_id) f;
---
--- SELECT f.* FROM fdwes.z_text t, es_search(t.query, t.pg_id) f;
-SELECT COUNT(*) FROM fdwes.z_text;
--- CREATE MATERIALIZED VIEW fdwes.z_final_body AS
---     -- nesting the array with the different terms in its proper place
--- SELECT
---        row_id AS row_id_source,
---        json_build_object(
---            'query', json_build_object(
---                'bool', jsonb_build_object(
---                    'should',  jsar
---                )
---            )
---        ) as body
--- FROM elasticqueries.c_array_wo_nulls;
---
--- CREATE TABLE fdwes.raw_results (
---     pg_id INTEGER,
---     query TEXT,
---     response TEXT
--- );
---
---
---
--- CREATE TABLE foo as
--- SELECT fdwes.es_search(body::TEXT, row_id_source) as b
--- FROM fdwes.z_final_body LIMIT 5;
-
---
--- INSERT INTO fdwes.raw_results
--- SELECT b.pg_id, b.query, b.response from (SELECT fdwes.es_search(body::TEXT, row_id_source) FROM fdwes.z_final_body LIMIT 5) b;
---
---
+SELECT t.pg_id, t.query, c.response
+FROM (SELECT * FROM fdwes.dsl_query ) t
+CROSS JOIN LATERAL (SELECT * FROM fdwes.dsl_target b WHERE b.query =t.query LIMIT 7) c;
---
+++ dockersetup/shared_volume/docker_pg_es.sh
@@ -0,0 +1,7 @@
+# docker exec postgres /bin/bash -c "cd /multicorn_fdw_for_es/suricate_fdw; pip install --upgrade ."
+docker exec postgres /bin/bash -c "cd /multicorn_fdw_for_es/pg_es_fdw; pip install --upgrade ."
+cd /Users/pogier/Documents/63-Elk/dockersetup
+docker stop postgres
+docker-compose up -d
+sleep 3
+docker exec -it postgres psql -U myuser -d mydb -f /shared_volume/3_create_suricate_fdw.sql
